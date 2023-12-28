722b324aca51e4cf6b15c53282b713bbb77350d5
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-12-28 16:58:34 +0100

    set up boilerplate

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// when starting the server. Example command to overwrite PORT env variable value:
+// PORT=9000 npm start
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
+
+// Documents my API
+app.get("/", (_, res) => {
+    res.json(listEndpoints(app));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
