10486484c0863214d44f35cad5f659bccdbaec59
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2024-01-04 17:42:47 +0100

    Defined DELETE list method

--- backend/routes/ListRoutes.js
+++ backend/routes/ListRoutes.js
@@ -15,7 +15,7 @@ listRoutes.get("/lists", verifier, async (req, res) => {
       res.status(404).json({ message: "User not found" });
       // If the user exists, respond with the user's lists
     } else {
-      res.status(200).json({ lists: user.lists });
+      res.status(200).json(user.lists);
     }
     // If something goes wrong, respond with a 500 error
   } catch (error) {
@@ -39,6 +39,25 @@ listRoutes.post("/lists", verifier, async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Could not create list" });
+  }
+});
+
+// This route will delete a list
+listRoutes.delete("/lists/:id", verifier, async (req, res) => {
+  try {
+    // Get user from database
+    const user = await getUser(req.user.id);
+    // Remove the list from the array by filtering out the list with the specified id
+    user.lists = user.lists.filter(
+      (list) => list._id.toString() !== req.params.id,
+    );
+    await user.save();
+    // Respond with a 204 status code with no content
+    res.status(204).json();
+    // If something goes wrong, respond with a 500 error
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Could not delete list" });
   }
 });
 
