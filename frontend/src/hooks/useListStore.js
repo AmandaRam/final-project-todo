56250c63cd6fb920e98a278f80dcc6e48d173bc1
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2024-01-12 21:44:09 +0100

    Added one change to previous commit

--- frontend/src/hooks/useListStore.js
+++ frontend/src/hooks/useListStore.js
@@ -4,6 +4,12 @@ import { create } from "zustand";
 const useListStore = create((set) => ({
   lists: [],
   setLists: (lists) => set({ lists }),
+  removeTodo: (listId, todoId) => set((state) => {
+    list.todos = todos;
+    return { lists:[
+      ...state.lists.filter((list) => list.id !== listId)] };
+      state.lists[listId]
+  }
 }));
 
 export default useListStore;
